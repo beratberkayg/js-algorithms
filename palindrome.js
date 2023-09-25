09c6f34a8591419a1005a16d01bd25beae102a2b
beratberkayg <berat.1675@gmail.com>
2023-09-25 17:25:39 +0300

    update

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,11 @@
 const isPalindrome = (str) => {
   const reverse = str.split("").reverse().join("");
   if (str === reverse) {
-    console.log(str + "is a palindrome.");
+    console.log(str + " " + "is a palindrome.");
   } else {
-    console.log(str + "is not a palindrome.");
+    console.log(str + " " + "is not a palindrome.");
   }
 };
 
-console.log(isPalindrome(yapay));
-console.log(isPalindrome(berat));
+console.log(isPalindrome("yapay"));
+console.log(isPalindrome("berat"));
