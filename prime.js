6b58a953dd15b2df6c2c61bae8b6b550c841c8f6
beratberkayg <berat.1675@gmail.com>
2023-09-26 19:17:50 +0300

    update

--- prime.js
+++ prime.js
@@ -1,16 +1,15 @@
 function isPrime(n) {
-  if (n <= 1) return console.log(n + "is not prime");
+  if (n <= 1) return console.log(n + " " + "is not prime");
 
   for (let i = 2; i < n; i++) {
-    if (n % 2 == 0) {
-      console.log(n + "is not prime");
+    if (n % i == 0) {
+      return console.log(n + " " + "is not prime");
     } else {
-      console.log(n + "is prime");
+      return console.log(n + " " + "is prime");
     }
   }
 }
 
 isPrime(13);
-isPrime(12);
+isPrime(24);
 isPrime(1);
-isPrime(2);
